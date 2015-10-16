a9541ec354d35f5c53604c4b52edd69917d75bf9
Jeffery Yaw Owusu- Ansah <jyoansah@gmail.com>
2015-10-16 18:46:11 -0400

    Start of redesign for cleaner more anuimated website.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,40 @@
 $(document).ready(function () {
-    $("body").css("display", "none");
-    $("header").fadeIn(500);
-    $("body").fadeIn(700);
+    $("body").css("display", "none").fadeIn(1000);
+    $(".menu").button();
 
-    $("a.transition").click(function (event) {
+    $("button.menu").click(function (event) {
         event.preventDefault();
-        linkLocation = this.href;
-        $("body").fadeOut(500, redirectPage);
+        description = this.getAttribute('name');
+
+        $(".selected").toggleClass("selected");
+
+        if(description == "close"){
+            $(".title").css('margin-top', '15%');
+            $(".title h1").css('opacity','1');
+            $(".content").fadeOut(1000);
+            $(".content .desc").fadeOut(1000);
+            $(".content ." + description).fadeOut(1000);
+            $(".home").fadeOut(1000);
+
+        }else{
+
+            jQuery(this).toggleClass("selected");
+            $(".title").css('margin-top', '0');
+            $(".title h1").css('opacity','0');
+            $(".content").fadeIn(1000);
+            $(".content .desc").fadeOut(1000);
+            $(".content ." + description).fadeIn(1000);
+            $(".home").fadeIn(1000)
+
+        }
+
     });
 
     function redirectPage() {
         window.location = linkLocation;
     }
+
+
 });
+
+
